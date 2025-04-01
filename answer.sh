d0fe1c78927f96d6c87b53816e154a0f28fa307a
kanshi-matsunobu <kanshi0107@gmail.com>
2025-04-01 17:12:49 +0900

    answer.shの修正

--- answer.sh
+++ answer.sh
@@ -10,7 +10,7 @@ $ cd ~
 $ cd ~/geekationWorkspace
 
 # 4. `LinuxCommandPractice`というディレクトリを作成して、`LinuxCommandPractice`に移動してください。
-$ mkdir LinuxCommandPractice
+$ mkdir -v LinuxCommandPractice
 
 # 5. カレントディレクトリのパスを表示してください。
 $ pwd
@@ -28,8 +28,7 @@ $ git push -u origin main
 # ここまでGithubリポジトリ作成時の初期コマンド
 
 # 7. `LinuxCommandPractice`ディレクトリに`.git`ディレクトリが表示されていることを確認してください。
-$ ls
-README.md
+$ ls -la
 # 8. `LinuxCommandPractice`ディレクトリ内に`practice_dir_1`ディレクトリを作成してください。
 $ mkdir practice_dir_1
 
@@ -43,13 +42,13 @@ $ mkdir practice_dir_1/practice_dir_2
 $ ls practice_dir_1
 practice_dir_2
 # 12. `LinuxCommandPractice`ディレクトリ内から**移動せずに**、`./practice_dir_1/practice_dir_2`ディレクトリ内に`practice_file1.txt`、`practice_file2.txt`というファイルを**一回のコマンドで**作成してください。**但し、相対パスを使用してください。**
-$ touch practice_dir_1/practice_file1.txt practice_dir_2/practice_file2.txt
+$ touch ./practice_dir_1/practice_dir_2/practice_file1.txt ./practice_dir_1/practice_dir_2/practice_file2.txt
 
 # 13 `practice_dir_2`に移動してください。**但し、相対パスを使用してください。**
 $ cd practice_dir_1/practice_dir_2
 
 # 14. `practice_file1.txt`を2つ上の階層のディレクトリ(`LinuxCommandPractice`)へ**移動**してください。**但し、相対パスを使用してください。**
-$ mv practice_dir_1/practice_file1.txt ../../
+$ mv ./practice_file1.txt
 
 # 15. `practice_file2.txt`を2つ上の階層のディレクトリ(`LinuxCommandPractice`)へ**コピー**してください。**但し、相対パスを使用してください。**
 $ cp practice_file2.txt ../../
